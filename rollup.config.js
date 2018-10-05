9ea24b363be390372421d265047b4a887737017c
Maurice de Laat <info@mauricedelaat.nl>
2018-10-05 16:56:19 +0200

    Fixed rollup.config.js.

--- rollup.config.js
+++ rollup.config.js
@@ -5,5 +5,5 @@ export default {
     name: '@yellicode/csharp',
     file: 'dist/bundles/csharp.umd.js'
   },
-  external: ['@yellicode/core', '@yellicode/model', '@yellicode/templating'] // https://github.com/rollup/rollup/wiki/Troubleshooting#treating-module-as-external-dependency
+  external: ['@yellicode/core', '@yellicode/elements', '@yellicode/templating'] // https://github.com/rollup/rollup/wiki/Troubleshooting#treating-module-as-external-dependency
 }
