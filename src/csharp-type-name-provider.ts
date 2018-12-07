a1cdeffc1c5ac584815e27476306a1c359088be2
Maurice de Laat <info@mauricedelaat.nl>
2018-12-07 13:16:11 +0100

    Implemented new DefaultTypeNameProvider functions (@yellicode/templating 1.1.0).

--- src/csharp-type-name-provider.ts
+++ src/csharp-type-name-provider.ts
@@ -3,18 +3,15 @@
 import { DefaultTypeNameProvider } from '@yellicode/templating';
 
 export class CSharpTypeNameProvider extends DefaultTypeNameProvider {
-    protected /*override*/ getDataTypeName(typedElement: elements.TypedElement): string | null {
-        if (!typedElement || !typedElement.type)
-            return null;
-        
-        const t = typedElement.type;
-        if (elements.isPrimitiveBoolean(t)) return "bool";
-        if (elements.isPrimitiveInteger(t)) return "int";
-        if (elements.isPrimitiveReal(t)) return "double"; // By default, a real numeric literal on the right side of the assignment operator is treated as double (https://msdn.microsoft.com/en-us/library/b1e65aza.aspx)
-        if (elements.isPrimitiveString(t)) return "string";
-        if (elements.isPrimitiveObject(t)) return "object";
-        
-        return super.getDataTypeName(typedElement);
+    protected /*override*/ getTypeNameForType(type: elements.Type | null, isDataType: boolean): string | null { 
+        if (!type) return null;
+
+        if (elements.isPrimitiveBoolean(type)) return "bool";
+        if (elements.isPrimitiveInteger(type)) return "int";
+        if (elements.isPrimitiveReal(type)) return "double"; // By default, a real numeric literal on the right side of the assignment operator is treated as double (https://msdn.microsoft.com/en-us/library/b1e65aza.aspx)
+        if (elements.isPrimitiveString(type)) return "string";
+        if (elements.isPrimitiveObject(type)) return "object";
+        return super.getTypeNameForType(type, isDataType);
     }
 
     public static canBeNullable(type: elements.Type | null): boolean {
