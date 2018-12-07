2ae2716ca904cc892f957cea090c625bb577804c
Maurice de Laat <info@mauricedelaat.nl>
2018-12-07 13:52:05 +0100

    Minor TypeNameProvider improvement.

--- src/csharp-type-name-provider.ts
+++ src/csharp-type-name-provider.ts
@@ -3,14 +3,17 @@
 import { DefaultTypeNameProvider } from '@yellicode/templating';
 
 export class CSharpTypeNameProvider extends DefaultTypeNameProvider {
-    protected /*override*/ getTypeNameForType(type: elements.Type | null, isDataType: boolean): string | null { 
+    protected /*override*/ getTypeNameForType(type: elements.Type | null, isDataType: boolean): string | null {
         if (!type) return null;
 
-        if (elements.isPrimitiveBoolean(type)) return "bool";
-        if (elements.isPrimitiveInteger(type)) return "int";
-        if (elements.isPrimitiveReal(type)) return "double"; // By default, a real numeric literal on the right side of the assignment operator is treated as double (https://msdn.microsoft.com/en-us/library/b1e65aza.aspx)
-        if (elements.isPrimitiveString(type)) return "string";
-        if (elements.isPrimitiveObject(type)) return "object";
+        if (isDataType) {
+            if (elements.isPrimitiveBoolean(type)) return "bool";
+            if (elements.isPrimitiveInteger(type)) return "int";
+            if (elements.isPrimitiveReal(type)) return "double"; // By default, a real numeric literal on the right side of the assignment operator is treated as double (https://msdn.microsoft.com/en-us/library/b1e65aza.aspx)
+            if (elements.isPrimitiveString(type)) return "string";
+            if (elements.isPrimitiveObject(type)) return "object";
+        }
+
         return super.getTypeNameForType(type, isDataType);
     }
 
